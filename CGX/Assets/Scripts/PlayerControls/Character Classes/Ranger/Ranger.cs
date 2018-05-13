f6f2d49537683dddea218607b53f386e002a3546
Logan Stewart <stewart.jlt@gmail.com>
2018-05-13 11:25:08 -0500

    Soundsmk1

--- CGX/Assets/Scripts/PlayerControls/Character Classes/Ranger/Ranger.cs
+++ CGX/Assets/Scripts/PlayerControls/Character Classes/Ranger/Ranger.cs
@@ -28,16 +28,19 @@ class Ranger : PlayerClass
                 break;
             case 1: //run
                 _animator.Play(Animator.StringToHash("RangerRun"));
+                AkSoundEngine.PostEvent("Ranger_Footsteps", gameObject);
                 AnimationEnd();
                 break;
             case 2: //jump up
                 _animator.Play(Animator.StringToHash("RangerJump"));
+                AkSoundEngine.PostEvent("Ranger_Jump", gameObject);
                 break;
             case 3: //fall down
                 _animator.Play(Animator.StringToHash("RangerFall"));
                 break;
             case 4: //ability
                 _animator.Play(Animator.StringToHash("RangerPower"));
+                AkSoundEngine.PostEvent("Ranger_Attack", gameObject);
                 break;
             case 5: //fainting
                 break;
